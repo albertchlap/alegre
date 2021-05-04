8f92b383abd45c8d1d80e4b332e65a35b8763b9c
Albert Chlapowski <chlapowski.0404@gmail.com>
2021-05-04 15:32:00 -0400

    fix to g-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,7 +56,7 @@ module.exports = {
       resolve: `gatsby-source-strapi`,
       options: {
         apiURL: process.env.DEPLOY_URL
-          ? process.env.DEPLOY_URL
+          ? "https://alegreimports.herokuapp.com"
           : "http://localhost:1337",
         queryLimit: 5000, // Default to 100
         contentTypes: [
